e56172de5016e36f997ef6249e17e3fad864674a
haendelf <123293142+haendelf@users.noreply.github.com>
2024-03-13 05:43:08 +0100

    Update const.ts

--- src/app/chat/features/Migration/const.ts
+++ src/app/chat/features/Migration/const.ts
@@ -6,8 +6,8 @@ export enum UpgradeStatus {
   UPGRADE_FAILED,
 }
 
-export const V1DB_NAME = 'LobeHub';
-export const V1DB_TABLE_NAME = 'LOBE_CHAT';
+export const V1DB_NAME = 'Polimat';
+export const V1DB_TABLE_NAME = 'Polimat_CHAT';
 
 export interface MigrationError {
   message: string;
